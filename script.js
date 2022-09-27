c9ff00f96e54d6d67004d2fb7f01dcece450bd08
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-09-27 09:56:05 -0500

    modified stylings and added code

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ const addToCart = document.querySelector("#addToCart");
 var items = document.getElementsByTagName("li");
 var itemList = document.querySelector("#addLi");
 
-const emptyCartMessage = document.createElement("p");
-emptyCartMessage.innerHTML = "Empty cart";
-addToCart.appendChild(emptyCartMessage);
-
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", (e) => {
     cartNumbers();
@@ -18,6 +14,18 @@ for (let i = 0; i < btn.length; i++) {
     const buttonRemove = document.createElement("button");
     cart.classList.add("cartLi");
     buttonRemove.className = "remove";
+
+    // for (let i = 0; i < cart.childNodes.length; i++) {
+    //   if (cart.childNodes[i].className === "cartLi") {
+    //     console.log("4444", cart.childNodes[i].firstChild.textContent.trim());
+    //     if (
+    //       e.target.parentElement.firstChild.textContent.trim() ===
+    //       cart.childNodes[i].firstChild.textContent.trim()
+    //     ) {
+    //       alert("item already added to your cart");
+    //     }
+    //   }
+    // }
 
     if (textIn.value) {
       cart.appendChild(document.createTextNode(`${textIn.value}`));
@@ -32,21 +40,29 @@ for (let i = 0; i < btn.length; i++) {
     buttonRemove.appendChild(document.createTextNode("Remove"));
     cart.appendChild(buttonRemove);
 
+    var itemNumbers = document.querySelector("#addToCart span");
+    for (let i = 0; i < addToCart.childNodes.length; i++) {
+      if (addToCart.childNodes[i].className === "cartLi") {
+        console.log("4444", addToCart.childNodes[i].firstChild);
+        if (
+          e.target.parentElement.firstChild.textContent.trim() ===
+          addToCart.childNodes[i].firstChild.textContent.trim()
+        ) {
+          alert(
+            `"${e.target.parentElement.firstChild.textContent.trim()}" already added to your cart`
+          );
+          itemNumbers.textContent -= 1;
+          return;
+        }
+      }
+    }
+
     addToCart.appendChild(cart);
-    // if (
-    //   cart.firstChild.textContent ===
-    //   e.target.parentElement.firstChild.textContent
-    // ) {
-    //   alert("Already added");
-    // }
-    // console.log(cart.firstChild.textContent);
-    // console.log(e.target.parentElement.firstChild.textContent);
-    emptyCartMessage.className = "hide-empty-cart";
 
     textIn.value = "";
   });
 }
-// btn.addEventListener("click", addList);
+
 itemList.addEventListener("click", removeItem);
 addToCart.addEventListener("click", removeItem);
 
@@ -61,46 +77,25 @@ function removeItem(e) {
       const ol = li.parentNode;
       ol.removeChild(li);
       decreaseCartNumbers();
-
-      var currentCartItems = document.getElementsByClassName("cartLi");
-      if (currentCartItems.length <= 0) {
-        emptyCartMessage.classList.remove("hide-empty-cart");
-      }
     }
   }
 }
 
-var counter = 0;
-
 function cartNumbers() {
   var itemNumbers = document.querySelector("#addToCart span");
   if (itemNumbers) {
-    counter++;
-    itemNumbers.textContent = counter;
-    console.log(itemNumbers.textContent);
+    itemNumbers.textContent++;
   }
 }
 
 function decreaseCartNumbers() {
   var itemNumbers = document.querySelector("#addToCart span");
   if (itemNumbers) {
-    counter--;
-    itemNumbers.textContent = counter;
+    itemNumbers.textContent--;
     console.log(itemNumbers.textContent);
   }
 }
 
-const emptyCart = document.getElementById("delete");
-var currentCartItems = document.getElementsByClassName("cartLi");
-var itemNumbers = document.querySelector("#addToCart span");
-emptyCart.addEventListener("click", () => {
-  for (let i = 0; i < currentCartItems.length; i++) {
-    currentCartItems[i].style.display = "none";
-    counter = 0;
-    itemNumbers.textContent = counter;
-    emptyCartMessage.classList.remove("hide-empty-cart");
-  }
-});
 // ------------ For LocalStorage ------------ //
 
 // function onLoadCartNumbers() {
